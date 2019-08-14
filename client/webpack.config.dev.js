059693871b7c8e274c3cdc8c025ccb4e3459513a
Elliot Davies <elliot.a.davies@gmail.com>
2019-08-14 22:24:36 +0100

    Webpack > Rollup tbh

---
+++ client/webpack.config.dev.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/main.js",
+
+  devServer: {
+    inline: true,
+    hot: true,
+    open: true,
+    port: 8080,
+    stats: "minimal"
+  },
+
+  devtool: "cheap-module-inline-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.(html|svelte)$/,
+        exclude: [/node_modules/, /index.html/],
+        use: {
+          loader: "svelte-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      templateParameters: {
+        apiUrl: "http://localhost:8000"
+      }
+    })
+  ]
+};
---
+++ client/webpack.config.prod.js
@@ -0,0 +1,61 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+
+  entry: "./src/main.js",
+
+  devtool: "none",
+
+  output: {
+    filename: "[contenthash].[name].js",
+    pathinfo: false
+  },
+
+  stats: {
+    children: false,
+    modules: false
+  },
+
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(html|svelte)$/,
+        exclude: [/node_modules/, /index.html/],
+        use: {
+          loader: "svelte-loader",
+          options: {
+            emitCss: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      templateParameters: {
+        apiUrl: "%%API_URL%%"
+      },
+      favicon: "assets/favicon.png"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].[name].css"
+    })
+  ]
+};
